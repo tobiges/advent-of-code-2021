ee199e66f8cc2574a4384191248915313289adba
Tobias Gessler <gessler.tobi@gmail.com>
2021-12-09 00:56:42 +0100

    day8

---
+++ src/day08.rs
@@ -0,0 +1,63 @@
+pub fn part1(input: String) {
+    let answer = input
+        .lines()
+        .flat_map(|line| line.split_once('|').unwrap().1.split_ascii_whitespace())
+        .fold(0, |acc, num| match num.len() {
+            2 | 3 | 4 | 7 => acc + 1,
+            _ => acc,
+        });
+    println!("Answer: {}", answer);
+}
+
+pub fn part2(input: String) {
+    let answer: u32 = input.lines().map(parse_line).map(solve).sum();
+    println!("Answer: {}", answer);
+}
+
+fn parse_line(combination: &str) -> Vec<u8> {
+    combination
+        .split(&[' ', '|'][..])
+        .filter(|comb| !comb.is_empty())
+        .map(|comb| comb.chars().fold(0, |acc, c| acc | 1 << c as u8 - b'a'))
+        .collect()
+}
+
+#[derive(Default)]
+struct SolveInfo {
+    one: u8,
+    four_without_one: u8,
+}
+
+fn solve(line: Vec<u8>) -> u32 {
+    let mut solve_info = SolveInfo::default();
+    for &comb in &line[0..10] {
+        match comb.count_ones() {
+            2 => solve_info.one = comb,
+            4 => solve_info.four_without_one = comb,
+            _ => (),
+        }
+    }
+    solve_info.four_without_one &= !solve_info.one;
+
+    let mut sum = 0;
+    for &digit in &line[10..14] {
+        sum = sum * 10 + solve_digit(&solve_info, digit);
+    }
+    sum
+}
+
+fn solve_digit(solve_info: &SolveInfo, digit: u8) -> u32 {
+    match digit.count_ones() {
+        2 => 1,
+        3 => 7,
+        4 => 4,
+        5 if digit & solve_info.one == solve_info.one => 3,
+        5 if digit & solve_info.four_without_one == solve_info.four_without_one => 5,
+        5 => 2,
+        6 if digit & solve_info.one != solve_info.one => 6,
+        6 if digit & solve_info.four_without_one != solve_info.four_without_one => 0,
+        6 => 9,
+        7 => 8,
+        _ => panic!("invalid one count"),
+    }
+}
